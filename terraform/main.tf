f1ce2378affd677497fa78eef299bbeaf09a2205
alarav <lara.arnoldandres@gmail.com>
2025-08-15 10:48:54 -0500

    Add a random value to concat with the name of all resources and generate a local file to save app_name and group_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
+resource "random_id" "unique_suffix" {
+  byte_length = 4
+}
 
 resource "azurerm_resource_group" "devops" {
-  name     = "prod-website-devops-rg"
+  name     = "prod-website-devops-${random_id.unique_suffix.hex}-rg"
   location = var.resource_group_location
 }
 
 resource "azurerm_service_plan" "devops" {
-  name                = "prod-website-devops-plan"
+  name                = "prod-website-devops-${random_id.unique_suffix.hex}-plan"
   resource_group_name = azurerm_resource_group.devops.name
   location            = var.resource_group_location
   os_type             = "Linux"
@@ -13,7 +16,7 @@ resource "azurerm_service_plan" "devops" {
 }
 
 resource "azurerm_linux_web_app" "devops" {
-  name                = "prod-website-devops"
+  name                = "prod-website-devops-${random_id.unique_suffix.hex}-app"
   resource_group_name = azurerm_resource_group.devops.name
   location            = var.resource_group_location
   service_plan_id     = azurerm_service_plan.devops.id
@@ -30,4 +33,12 @@ output "app-name" {
 
 output "group-name" {
   value = azurerm_resource_group.devops.name
+}
+
+resource "local_file" "app_details" {
+  content = <<-EOT
+    app_name=${azurerm_linux_web_app.devops.name}
+    group_name=${azurerm_resource_group.devops.name}
+  EOT
+  filename = "${path.module}/app_details.txt"
 }
